3f995a77818c611a5654a93b16fa9d0efc5b0126
Salvador Alvarado Diaz <salvalva@c1r1s3.42malaga.com>
2023-06-12 19:59:40 +0200

    final repo 12/06/2023

--- ft_split.c
+++ ft_split.c
@@ -6,52 +6,52 @@
 /*   By: salvalva <salvalva@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 08:28:00 by salvalva          #+#    #+#             */
-/*   Updated: 2023/06/09 08:46:37 by salvalva         ###   ########.fr       */
+/*   Updated: 2023/06/12 19:56:07 by salvalva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_countword(const char *s, char c)
+static size_t	ft_wordcounter(char const *s, char c)
 {
-	size_t	wrd;
+	size_t	i;
+	size_t	x;
 
-	wrd = 0;
-	while (*s)
+	i = 0;
+	x = 0;
+	while (s[i] != '\0')
 	{
-		if (*s != c)
-		{
-			wrd++;
-			while(*s && *s != c)
-				s++;
-		}
-		else
-			s++;
+		if (s[i++] != c && (s[i] == c || s[i] == '\0'))
+		x++;
 	}
-	return (wrd);
+	return (x);
 }
 
-char	**ft_split(const char *s, char c)
+char	**ft_split(char const *s, char c)
 {
-	char 	**str;
-	size_t	i;
+	char	**str;
 	size_t	len;
-	
-	if (!s)
-		return (NULL);
-	i = 0;
-	str = (char *)malloc(sizeof(char) * (ft_countword(s, c) + 1));
+	size_t	n;
+
+	str = malloc(sizeof(char *) * (ft_wordcounter (s, c) + 1));
+	if (!str)
+		return (0);
+	n = 0;
 	while (*s)
 	{
 		if (*s != c)
 		{
 			len = 0;
-			while (*s && *s != 0 && ++len)
+			while (*s && *s != c)
+			{
+				len++;
 				s++;
-			str[i++] = ft_substr(s - len, 0, len);
+			}
+			str[n++] = ft_substr(s - len, 0, len);
 		}
 		else
-			s++;
+		s++;
 	}
+	str[n] = 0;
 	return (str);
 }
